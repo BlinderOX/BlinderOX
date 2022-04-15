071c56260f9fa5cadfc95b71a2cfb4af8d7726d9
BlinderOX <102313067+BlinderOX@users.noreply.github.com>
2022-04-15 11:45:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -298,7 +298,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('Join Our Update Channel', url=f'https://t.me/MovieSearcherXBotUpdates')
                     ]
                     ]
                 
@@ -329,7 +329,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{title}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('Join Our Update Channel', url=f'https://t.me/MovieSearcherXBotUpdates')
                     ]
                     ]
                 
